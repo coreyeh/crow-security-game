d75d6d15acc948cba12f9bf1a7335333d26eaba4
coreyeh <coreyerbhastie@gmail.com>
2025-06-22 22:21:42 +1200

    Amplify Outputs + Directory Path Alias

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
+import * as path from "path";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
   ],
+  resolve: {
+    alias: {
+      '@@': __dirname,
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 })
