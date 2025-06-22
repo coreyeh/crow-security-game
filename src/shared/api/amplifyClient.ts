e36c7a125a999d3d96d61f28d6594545edebcac4
coreyeh <coreyerbhastie@gmail.com>
2025-06-22 22:35:45 +1200

    Amplify Client + Feature-Sliced-Design (FSD)

---
+++ src/shared/api/amplifyClient.ts
@@ -0,0 +1,12 @@
+
+import type { Schema } from '@@/amplify/data/resource';
+import { generateClient } from 'aws-amplify/data';
+
+/**
+ * This module provides a pre-configured instance of the Amplify Data client.
+ *
+ * Do not import generateClient directly in your features or entities.
+ * Instead, use this exported `client` instance to ensure consistent configuration
+ * and avoid duplication of client logic across the codebase.
+ */
+export const client = generateClient<Schema>();
