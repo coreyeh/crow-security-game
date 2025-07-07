67c87e5cd69e8033d721624cc8ee815a3a178e41
coreyeh <coreyerbhastie@gmail.com>
2025-07-07 17:25:08 +1200

    Navbar UserMenu Dropdown

--- src/features/auth/ui/UserMenu.tsx
+++ src/features/auth/ui/UserMenu.tsx
@@ -1,22 +1,54 @@
 import { useAuthenticator } from '@aws-amplify/ui-react';
-import { FaUserCircle, FaCaretDown } from 'react-icons/fa';
+import { FaUserCircle, FaCaretDown, FaCaretUp, FaUser, FaCog, FaSignOutAlt } from 'react-icons/fa';
 
 export default function UserMenu() {
-  const { user, /*signOut,*/ authStatus } = useAuthenticator((context) => [context.user, context.authStatus]);
-  
-  const userLabel = authStatus === 'authenticated' 
-    ? user?.username
-    : 'Guest';
+  const { user, signOut, authStatus } = useAuthenticator((context) => [context.user, context.authStatus]);
+  const isAuthenticated = authStatus === 'authenticated';
+
+  const handleAuthentication = () => {
+    if (isAuthenticated) {
+      signOut();
+      return;
+    }
+
+    /* Redirect to login or registration page */
+  }
 
   return (
-    <>
-      <button className="cursor-pointer flex items-center justify-center rounded-md bg-darker hover:bg-dark gap-2 py-2 px-4">
+    <div className="relative">
+      <button className="dropdown-button cursor-pointer flex items-center justify-center gap-2 py-2 px-4 rounded-md bg-darker hover:bg-dark">
         <FaUserCircle className="text-3xl text-lightest" />
-        <div className="flex items-center gap-1">
-          <span className="text-sm text-lightest">{userLabel}</span>
-          <FaCaretDown className="text-md text-lightest" />
-        </div>
+        <span className="text-sm text-lightest">
+          {isAuthenticated ? user?.username || 'User' : 'Guest'}
+        </span>
+        <FaCaretDown className="dropdown-closed text-md text-lightest" />
+        <FaCaretUp className="dropdown-open hidden text-md text-lightest" />
       </button>
-    </>
+      <menu className="dropdown-menu absolute right-0 top-full flex-col gap-2 min-w-48 mt-4 p-2 rounded-md bg-darker shadow-md shadow-darkest">
+        <li className="rounded hover:bg-darkest text-lightest hover:text-foreground">
+          <button className="cursor-pointer flex items-center text-left w-full gap-4 p-2">
+            <FaUser className="text-md" />
+            <span className="text-sm">Account</span>
+          </button>
+        </li>
+        <li className="rounded hover:bg-darkest text-lightest hover:text-foreground">
+          <button className="cursor-pointer flex items-center text-left w-full gap-4 p-2">
+            <FaCog className="text-md" />
+            <span className="text-sm">Settings</span>
+          </button>
+        </li>
+        <li className="rounded hover:bg-darkest text-lightest hover:text-foreground">
+          <button 
+            onClick={handleAuthentication} 
+            className="cursor-pointer flex items-center text-left w-full gap-4 p-2"
+          >
+            <FaSignOutAlt className="text-md" />
+            <span className="text-sm">
+              {isAuthenticated ? 'Sign Out' : 'Register/Login'}
+            </span>
+          </button>
+        </li>
+      </menu>
+    </div>
   );
 }
--- src/widgets/Navbar/Navbar.tsx
+++ src/widgets/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import UserMenu from '@/features/auth/ui/UserMenu';
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between border-b border-dark p-2">
+    <nav className="flex items-center justify-between p-2 border-b border-dark">
       <header className="flex items-center gap-2">
         <img src="/brandmark.svg" alt="Brandmark" className="h-7 w-auto" />
         <img src="/logotype.svg" alt="Logotype" className="h-3 w-auto" />
