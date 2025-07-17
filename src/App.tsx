b6b31a2769490ee8cd556f228b5a1fd00583acad
coreyeh <coreyerbhastie@gmail.com>
2025-07-17 17:52:58 +1200

    React Router + React Icons + More Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
+
+import { Home } from './pages/home';
+import { Register } from './pages/register';
+import { Login } from './pages/login';
+
 function App() {
   return (
-    <>
-    </>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/login' element={<Login />} />
+    </Routes>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import { BrowserRouter } from 'react-router-dom'
+
 import './index.css'
 import App from './App.tsx'
 
@@ -11,6 +13,8 @@ Amplify.configure(outputs)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
